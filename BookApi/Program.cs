d488de2cf2832a8c6c5fbecc9f3da6cb82caa8f2
skolden1 <skolden123@hotmail.com>
2025-05-11 16:12:29 +0200

    Add project files.

---
+++ BookApi/Program.cs
@@ -0,0 +1,37 @@
+namespace BookApi
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            //Här kan vi lägga till services
+            builder.Services.AddControllers();
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("Mycors", builder =>
+                {
+                    builder.WithOrigins("http://localhost:4200")
+                    .AllowAnyHeader()
+                    .AllowAnyMethod();
+                });
+            });
+
+            var app = builder.Build();
+
+            app.UseCors("Mycors");
+
+            //Här kan vi lägga till mapping
+            app.MapControllers();  //Så fort en httpreq skickas så känner denna kod av alla httpget/httpost attributer
+            //Och då letar han upp rätt get/postmetod så att dom körs
+
+            app.MapGet("/", () =>
+            {
+                return Results.Redirect("/api/books"); //om man är på default page så blir man redirectad till denna adress ist
+            });
+
+            app.Run();
+        }
+    }
+}
